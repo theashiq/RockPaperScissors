8d4bb05434113930f7b8a6d9107e0e614ec9ceb2
Mac Mini <>
2023-12-19 00:39:04 +0600

    Project Created

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by mac 2019 on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
